f4e18834c7572e405981f34ca29c72acd3ed90b6
GoteGuru <meszaros.gergely@gmail.com>
2022-05-17 21:50:20 +0200

    getter for renderInfo, generatorInfo

--- common/package.js
+++ common/package.js
@@ -13,18 +13,28 @@ export default class Package {
     this.description = normalize(init.description)
   }
 
-  async loadRenderer(name) {
+  rendererInfo(name) {
     const rend = this.renderers.find(x => x.name === name)
     if (!rend) throw new Error(`No such renderer in ${this.name}: ${name}`)
+    return rend
+  }
+
+  async loadRenderer(name) {
+    const rend = this.rendererInfo(name)
     const from = rend?.src.from ? rend.src.from : `renderers/${name}.js`
     const tab = rend?.src.import ? rend.src.import : 'default'
     const mod = await import(`../packages/${this._path}/${from}`)
     return mod[tab]
   }
 
-  async loadGenerator(name) {
+  generatorInfo(name) {
     const gen = this.generators.find(x => x.name === name)
     if (!gen) throw new Error(`No such generator in ${this.name}: ${name}`)
+    return gen
+  }
+
+  async loadGenerator(name) {
+    const gen = this.generator(name)
     const from = gen?.src.from ? gen.src.from : `generators/${name}.js`
     const tab = gen?.src.import ? gen.src.import : 'default'
     const mod = await import(`../${this._path}/${from}`)
