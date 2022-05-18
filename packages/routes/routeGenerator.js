757771b580c3129919f271b08bb550f32b4e2dcc
GoteGuru <meszaros.gergely@gmail.com>
2022-05-19 00:07:12 +0200

    wip reouteGenerator scratch

--- packages/routes/routeGenerator.js
+++ packages/routes/routeGenerator.js
@@ -8,20 +8,49 @@ export default generatorContext => {
   // Resource can be missing two ways: if it's not generated yet or usage is
   // denied by the user (set to ingore).
 
-  cellGrid = generatorContext.resource('CellGrid')
-  burgs = generatorContext.resource('Burgs')
-  sea = generatorContext.resource('Sea')
-  rivers = generatorContext.resource('Rivers') // optional!
-  control = generatorContext.resource('ControlZones') // optional!
-  economy = generatorContext.resource('Economy') // optional!
-  routes = generatorContex.resource('Routes') // we can re-use existing data!
+  res = generatorContext.resources
+  /* This generator uses:
+    'Scale',    // mandatory, the Map's scale
+    'CellGrid', // mandatory
+    'Burgs',    // mandatory
+    'Sea',      // mandatory
+    'Rivers',   // optional
+    'ControlZones', // optional
+    'Economy',  // optional
+    'Cultures', // optional
+    'Routes',   // optional, we can re-use existing route data!
+    */
+
+
+  /* extract options from the context
+   * levels must be a Map() indexed by levelname, providing:
+   * {
+   * cost: Number,      # cost of building this kind of road
+   * minScale: Number,  # minimum map scale to generate this kind of road
+   * bridges: boolean,  # can this type of road can cross rivers
+   * useRiver: boolean, # this type can use rivers
+   * useLand: boolean,  # this type can cross land cells
+   * useSea: boolean,   # this type can cross sea cells
+   * useMountain: boolean, # this type can cross mountains
+   * }
+  */
+  const { levels,   } = generatorContext.config
 
   // find available land cells
-  const land = cellGrid.cellSet.new_difference(water.cellSet)
+  const land = res.cellGrid.cellSet.new_difference(water.cellSet)
 
   // if we have no-route control zones exclude them from the land set
-  if (control && control.noRoutes) land.difference(control.noRoutes)
+  if (control && control.noRoutes) land.difference(res.ControlZones.noRoutes)
 
+
+  // if we don't have any routes generate some basis.
+  let routes = [];
+  if (!res.Routes) {
+    routes = generateBaseRoutes(generatorContext)
+  } else {
+    routes
+
+  }
   // use the Burgs API to iterate over burgs, burgs are required, we don't
   // have to check its availability.
   const burgs.cellSet
@@ -35,4 +64,10 @@ export default generatorContext => {
   waterCells = sea.set()
 
 
+}
+
+function generateBaseRoutes(ctx) {
+  ctx.timer("Baseroute")
+    // we do nothing, but it's quite fast. :-D
+  ctx.timerEnd("Baseroute")
 }
--- packages/stateManager.js
+++ packages/stateManager.js
@@ -7,14 +7,31 @@ for generators.
 import { packages } from './registry.js'
 
 class GeneratorContext {
-  constructor(generatorClass) {
-    this.state = []
+  constructor(package, generatorName, mapState, config) {
+    this.info = package.generatorInfo(generatorName)
+    const { requires, uses, provides } = this.info
+
+    // all required details must be present in mapState
+    for (detail of requires)
+      if (!mapState.has(detail))
+        throw new Error(`${package}:${generatorName} requires ${detail}`)
+
+    this.resources = mapState
+    this.config = config
+    this.name = generatorName
   }
 
   // return resource name
-  resource(resourceName) {
+  getResources(...resourceNames) {
 
   }
 
+  timer(name) {
+    console.time(`${this.name}:${name}`);
+  }
+
+  endTimer(name) {
+    console.timeEnd(`${this.name}:${name}`)
+  }
 
 }
